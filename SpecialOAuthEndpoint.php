5673ff63d3fcefee9f43cba5b66f61855d7e8d07
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-11-02 18:12:24 -0700

    Refactor.

--- SpecialOAuthEndpoint.php
+++ SpecialOAuthEndpoint.php
@@ -18,15 +18,26 @@ class SpecialOAuthEndpoint extends SpecialPage {
     }
 
 
+    public function shouldRedirectToIdentityProvider(){
+
+        return empty($_GET["code"]);
+    }
+
+    public function authorizationCodeGranted(){
+
+        return !empty($_GET["code"]);
+    }
+
+
     public function execute($parameter) {
 
-        global $wgScriptPath, $wgRequest, $wgUser, $oauth_config;
+        global $oauth_config;
 
         $config = new OAuthConfig($oauth_config);
 
-        $credentialsAccepted = !empty($_GET["code"]);
+        if($this->authorizationCodeGranted()) $config->setAuthorizationCode($_GET["code"]);
 
-        if(!$credentialsAccepted) {
+        if($this->shouldRedirectToIdentityProvider()) {
         
             $response = OAuth::newOAuthResponse($config, "webserver");
 
@@ -35,10 +46,7 @@ class SpecialOAuthEndpoint extends SpecialPage {
             // Redirect to the salesforce login page.
             header("Location: $url");
 
-        } else {
-
-            $config->setAuthorizationCode($_GET["code"]);
-            unset($_GET["code"]);
+        } else if($this->authorizationCodeGranted()){
         
             // Build the request and send the authorization code returned in the previous step.
             $oauth = OAuthRequest::newAccessTokenRequest($config, "webserver");
@@ -46,33 +54,74 @@ class SpecialOAuthEndpoint extends SpecialPage {
             $resp = $oauth->authorize();
         
             $sfUserInfo = $this->getUserInfo($resp->getAccessToken(), $resp->getInstanceUrl());
-            $username = ucfirst($sfUserInfo["preferred_username"]);
+            $username = $this->formatMWUsername($sfUserInfo["preferred_username"]);
             $email = $sfUserInfo["email"];
 
-            $userFactory = MediaWikiServices::getInstance()->getUserFactory();
+            $user = !$this->userExists($username) ? $this->createUser($username, $email) : $this->loadUser($username);
 
-            $user = $userFactory->newFromName($username);
-            $user->load();
+            $this->logUserIn($user);
 
-            if($user->getId() == 0) {
-
-                $user = User::createNew($username, array()); // Add the user to the database and return user object.
-                $user->setRealName($username);
-                $user->setEmail($email);
-                $user->setToken();  // Set the random token (used for persistent authentication)
-            }
-
-            $user->setCookies();
-            $user->saveSettings();
-            $wgUser = $user;
-
-            $protectedRedirect = $wgRequest->getSessionData("redirect");
-            $redirect = !empty($protectedRedirect) ? $protectedRedirect : "Main_Page";
+            $url = $this->getRedirect();
     
-            header("Location: $wgScriptPath/index.php/$redirect");
+            header("Location: $url");
 		}
     }
 
+    public function formatMWUsername($username) {
+
+        return ucfirst($username);
+    }
+
+
+    public function getRedirect() {
+
+        global $wgRequest, $wgScriptPath;
+
+        $sessionRedirect = $wgRequest->getSessionData("redirect");
+
+        $redirect = !empty($sessionRedirect) ? $sessionRedirect : "Main_Page";
+
+        return "$wgScriptPath/index.php/$redirect";
+    }
+
+
+    public function userExists($username) {
+
+        $userFactory = MediaWikiServices::getInstance()->getUserFactory();
+        $user = $userFactory->newFromName($username);
+        $user->load();
+
+        return $user->getId() != 0;
+    }
+
+
+    public function loadUser($username){
+
+        $userFactory = MediaWikiServices::getInstance()->getUserFactory();
+        
+        return $userFactory->newFromName($username);
+    }
+
+
+    public function createUser($username, $email) {
+
+        $user = User::createNew($username, array()); // Add the user to the database and return user object.
+        $user->setRealName($username);
+        $user->setEmail($email);
+        $user->setToken();  // Set the random token (used for persistent authentication)
+
+        return $user;
+    }
+
+
+    public function logUserIn($user){
+
+        global $wgUser;
+
+        $user->setCookies();
+        $user->saveSettings();
+        $wgUser = $user;
+    }
 
 
     public function getUserInfo($accessToken, $instanceUrl){
--- includes/OAuthHooks.php
+++ includes/OAuthHooks.php
@@ -4,31 +4,75 @@ class OAuthHooks{
 
     public static $protected = array("Protected", "Test");
 
+    public static $oauthEndpoint = "Special:OAuthEndpoint";
+
+    public static $userLogout = "UserLogout";
+
+    public static $logoutRedirect = "Main_Page";
+
     public static function onBeforeInitialize( \Title &$title, $unused, \OutputPage $output, \User $user, \WebRequest $request, \MediaWiki $mediaWiki ) {
 
-        global $wgScriptPath;
+        // var_dump($title->mUrlform, self::$userLogout);exit;
 
-        $route = $output->getTitle()->mUrlform;
+        if($title->mUrlform == self::$userLogout) {
 
-        if(self::isProtected($route) && !self::isLoggedIn($user)){
-
-            $request->getSession()->persist();
-            $request->setSessionData("redirect", $route);
-
-            $url = "$wgScriptPath/index.php/Special:OAuthEndpoint";
+            $url = self::getLogoutRedirect();
             header("Location: $url");
 
             exit;
 
-        } else if($route == "UserLogout") {
-
-            $url = "$wgScriptPath/index.php/Main_Page";
-            header("Location: $url");
-
-        } else {
+        }else if(self::isPublic($title)){
 
             return;
+
+        } else if(!self::hasAccess($title, $user)){
+
+            $request->getSession()->persist();
+            $request->setSessionData("redirect", $title->mUrlform);
+
+            $url = self::getOAuthEndpoint();
+            header("Location: $url");
+
+            exit;
         }
+    }
+
+
+    public static function getOAuthEndpoint(){
+
+        global $wgScriptPath;
+
+        return "$wgScriptPath/index.php/" . self::$oauthEndpoint;
+    }
+
+    public static function getLogoutRedirect(){
+
+        global $wgScriptPath;
+
+        return "$wgScriptPath/index.php/" . self::$logoutRedirect;
+    }
+
+    public static function isProtected($title) {
+
+        return in_array($title->mUrlform, self::$protected);
+    }
+
+
+    public static function isPublic($title) {
+
+        return !in_array($title->mUrlform, self::$protected);
+    }
+
+
+    public static function hasAccess($title, $user) {
+
+        return self::isLoggedIn($user);
+    }
+
+    
+    public static function isLoggedIn($user) {
+
+        return $user->getId() != 0;
     }
 
 
@@ -52,17 +96,4 @@ class OAuthHooks{
 
 		return true;
 	}
-
-
-    public static function isProtected($route) {
-
-        return in_array($route, self::$protected);
-    }
-
-    
-    public static function isLoggedIn($user) {
-
-        return $user->getId() != 0;
-
-    }
 }
